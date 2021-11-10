f9819499f0c19d965cab833bf158b91b723bf29a
Ria Madhyan <ria.madhyan@db.com>
2021-11-10 18:26:44 +0530

    added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import React, {useEffect, useState} from 'react'
 import Advices from './components/Advices/Advices'
 
 const App = () => {
-  const [isTrue, setIsTrue] = useState(true); 
+  const [changeAdvice, setChangeAdvice] = useState(true); 
+  const [advice, setAdvice] = useState(null)
+
   useEffect(() => {
-    console.log("load");
-  }, []);
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(data => setAdvice(data))      
+      .catch(err => console.log(err))
+  }, [changeAdvice]);
 
   return (
-    <button onClick={() => setIsTrue(!isTrue)}>Click Me</button>
+    <>
+    <button onClick={() => setChangeAdvice(!changeAdvice)}>Click Me</button>
+    {advice && <Advices data={advice}/>}
+    
+    </>
   )
 }
 
--- src/components/Advices/Advices.jsx
+++ src/components/Advices/Advices.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 
 const Advices = (props) => {
-  const {advices} = props.data
+  const {advice} = props.data 
+  // https://api.adviceslip.com/#object-slip mentions that we need to use "advice" as key. Hence using props.data object & key advice or it can be done as below
+  // const {data} = props & then in line h2 can mention as {data.advice} instead of {advice}
+  
   return (
     <div>
-      <h2>{advices}</h2>
+      <h2>{advice}</h2>
     </div>
   )
 }
